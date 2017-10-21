50c6b511d9d276a1798447d420552db764f50da9
armin@syriana <armin@syriana>
2017-10-21 19:56:33 +0200

    add new stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles-debian
 Dotfiles repository for Debian based hosts
+
+# What is this?
+Just a collection of scripts that I use to initially set up some
+Debian based hosts.
+
---
+++ debian-initial-setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+basedir="$(dirname "$0")"
+
+# Helper functions
+info () {
+  echo
+  echo -e "$(tput setaf 4)[$(tput setaf 11)setup$(tput setaf 4)]$(tput setaf 6) $@ $(tput sgr0)"
+}
+
+fin () {
+  echo -e "$(tput setaf 4)[$(tput setaf 11)setup$(tput setaf 4)]$(tput setaf 6) done.$(tput sgr0)"
+}
+
+# Install packages
+info "Installing packages..."
+  packages=(screen lynx curl wget rsync tmux irssi vim-nox htop irssi git nano-)
+  apt update
+  apt install "${packages[@]}"
+fin
+
+# VIM stuff
+info "VIM configuration ..."
+if [[ -x ~/.vimrc ]]; then
+  echo "~/.vimrc found, not performing any changes to it."
+else
+  git clone https://github.com/netzverweigerer/config-vim.git /root/.vim && \
+    mkdir -p /root/.vim/swap && \
+    mkdir -p /root/.vim/backup
+fi 
+if [[ -x ~/.vim ]]; then
+  echo "~/.vim found, not performing any changes to it."
+else
+  git clone https://github.com/netzverweigerer/config-vim.git /root/.vim && mkdir -p /root/.vim/swap && mkdir -p /root/.vim/backup
+fi 
+fin
+
+echo
+
+# Dotfiles stuff
+for i in "$basedir"/dots/*; do
+  info "[dotfiles] Copying: $i"
+  cp -R -v "$i" "$HOME/.${i##*/}"
+done
+
+
+
